cad36b213570db4b4451fa01653e3c2a2e20f399
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2024-02-24 10:44:00 +0000

    Add files via upload

---
+++ if_condition1.py
@@ -0,0 +1,10 @@
+caracter = input("enter the caracter: ")
+
+if ("a"  <caracter and caracter < "b") or ("A"  <caracter and caracter < "B"):
+    print(caracter,"this an alphabet")
+
+if ("1" < caracter and caracter  <"9"):
+    print(caracter,"this is number :")
+
+else:
+    print(caracter,"this is speciale caracter") 
